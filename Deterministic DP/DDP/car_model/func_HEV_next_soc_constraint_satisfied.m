de7a38e29d0c0d785093372ed1f308703e4ede68
Yin <34277382+wadeKeith@users.noreply.github.com>
2023-05-05 18:51:23 +0800

    Delete Deterministic DP/DDP directory

--- Deterministic DP/DDP/car_model/func_HEV_next_soc_constraint_satisfied.m
+++
@@ -1,7 +0,0 @@
-function next_soc_is_valid = func_HEV_next_soc_constraint_satisfied(soc,motor_torque,torque_demand,rpm,HEV)
-    
-    next_soc = func_HEV_system_dynamics(soc,motor_torque,torque_demand,rpm,HEV);
-    
-    next_soc_is_valid = func_HEV_soc_constraint_satisfied(next_soc,HEV);
-    
-end
--- Deterministic DP/DDP/car_model/func_HEV_soc_constraint_satisfied.m
+++
@@ -1,8 +0,0 @@
-
-function soc_is_valid = func_HEV_soc_constraint_satisfied(soc,HEV)
-    if soc >= 0.2 && soc <= 0.8
-        soc_is_valid = true;
-    else
-        soc_is_valid = false;
-    end
-end
